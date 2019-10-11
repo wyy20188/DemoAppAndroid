1264803b5398d36f7c2550f5c38475a9f7bdb1bd
Yiyang <W>
2019-10-11 01:27:41 -0700

    Practice for IDP

---
+++ app/src/main/java/com/wyy/android/demo/BackUpActivity.kt
@@ -0,0 +1,11 @@
+package com.wyy.android.demo
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class BackUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+    }
+}
